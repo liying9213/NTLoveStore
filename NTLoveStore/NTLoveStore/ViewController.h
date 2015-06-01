3fff91d8e40e95b359e32e5c88a2f72b6cca10a0
liying <liying@xingshulin.com>
2015-06-01 11:41:07 +0800

    first create

---
+++ NTLoveStore/NTLoveStore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NTLoveStore
+//
+//  Created by 李莹 on 15/6/1.
+//  Copyright (c) 2015年 liying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
